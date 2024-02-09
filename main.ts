dd69e125706e19335b2e6cca017d0c618e183944
NikolaKufnerova <146720348+NikolaKufnerova@users.noreply.github.com>
2024-02-09 11:35:38 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
-const pole: Array<number> = [1, 2, 3, 4, 5]
+const pole: Array<number> = [1, 2, 3, 4, 5] //pole = []//
 
 let suma: number = 0
 for (let i = 0; 1 < pole.length; 1++) {
   suma += pole [1];
 }
-pole.push(6)
+
+pole.push(6) //metoda = funkce v globálním slope, navázaná na objekt = 'pole.push(6)'//
+
 console.log(pole)
 
-//metoda = funkce v globálním slope, navázaná na objekt = 'pole.push(6)'
+type Osoba = { //datové typy = boolean, array, number; type Osoba = definice pro definici, není to proměnná//
+  jmeno: string;
+  vek: number;
+}
+
+const osoba: Osoba = { 
+  jmeno: 'Petr',
+  vek: 25
+}
+
+const trida: Array<Osoba> = [];
+trida.push(osoba);
+trida.push({ jmeno: 'Karel', vek: 30 });
+
+console.table(trida) //table = tabulka//

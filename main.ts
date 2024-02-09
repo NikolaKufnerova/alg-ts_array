1a70b43e6d47bb6e52103ea088c2705c94b91a3c
NikolaKufnerova <146720348+NikolaKufnerova@users.noreply.github.com>
2024-02-09 11:21:57 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+const pole: Array<number> = [1, 2, 3, 4, 5]
+
+let suma: number = 0
+for (let i = 0; 1 < pole.length; 1++) {
+  suma += pole [1];
+}
+pole.push(6)
+console.log(pole)
+
+//metoda = funkce v globálním slope, navázaná na objekt = 'pole.push(6)'
